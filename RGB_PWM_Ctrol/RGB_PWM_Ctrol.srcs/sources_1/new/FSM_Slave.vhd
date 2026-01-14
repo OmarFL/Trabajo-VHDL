1a4eb8152aa30df39184b71df5050f430db6a212
HugoS13 <hugo.sierra@alumnos.upm.es>
2026-01-14 13:50:54 +0100

    CYCLES_SLOW

--- RGB_PWM_Ctrol/RGB_PWM_Ctrol.srcs/sources_1/new/FSM_Slave.vhd
+++ RGB_PWM_Ctrol/RGB_PWM_Ctrol.srcs/sources_1/new/FSM_Slave.vhd
@@ -67,7 +67,7 @@ architecture Behavioral of FSM_Slave is
 begin
 
     -- Selección del límite según la entrada del FSM_Master (opcional)
-    limit_val <= CYCLES_FAST when (DELAY_SELECT = '1') else CYCLES_TEST; --Ojo, he cambiado el lento por el test
+    limit_val <= CYCLES_FAST when (DELAY_SELECT = '1') else CYCLES_SLOW; --Ojo, he cambiado el lento por el test
 
 
     ----------------------------------------------------------------------------------
